030033e99a9511c7d9b806cb3a62681cea4feaf5
Erik de Jonge <erik@a8.nl>
2015-03-10 14:09:46 +0100

    pip Tuesday 10 March 2015 (week:10 day:69), 14:09:43

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='historybash',
-      version='4',
+      version='5',
       description='Bash history command colorized on levenshtein distance of last 10 commands',
       url='https://github.com/erikdejonge/historybash',
       author='Erik de Jonge',
