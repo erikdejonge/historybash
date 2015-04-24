34f21c1b18e3a83689a253019c7eea15275a0731
Erik de Jonge <erik@a8.nl>
2015-04-24 15:28:26 +0200

    rabshakeh Friday 24 April 2015 (week:16 day:114), 15:28:05

--- historybash/__init__.py
+++ historybash/__init__.py
@@ -7,15 +7,18 @@ Usage:
   history.py [options] [<keyword> <limitnum>]
 
 Options:
-  -i --id   Show id
+  -i --id    Show id
+  -r --run   Run id
   -h --help  Show this screen.
 """
-import docopt
 import hashlib
 
-from Levenshtein import distance
 from collections import deque
-from subprocess import PIPE, Popen, STDOUT
+
+import os
+import docopt
+import stat
+from Levenshtein import distance
 
 
 def get_distance(command, previous_command):
@@ -35,31 +38,24 @@ def main():
     """
     arguments = docopt.docopt(__doc__)
     showid = arguments["--id"]
+    runid = arguments["--run"]
     limitnum = arguments["<limitnum>"]
     keyword = arguments["<keyword>"]
 
+    if runid is True:
+        try:
+            keyword = int(keyword)
+        except ValueError:
+            print("\033[31minvalid number\033[0m", keyword)
+            return
+
     if keyword:
-        if len(keyword) == 0:
+        if len(str(keyword)) == 0:
             keyword = None
-    start = 0
-    if showid is True:
-        flags = "-i -l -c"
-        start = 4
-    else:
 
-        flags = "-i -c"
-
-    shell_command = 'bash ' + flags + ' "history -r; history"'
-
-    if keyword:
-        shell_command = 'bash ' + flags + ' "history -r; history|grep ' + keyword + '"'
-
-    event = Popen(shell_command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    sto, ste = event.communicate()
-    sto = sto.decode()
     previous_command = ""
     prev_cmds = deque()
-
+    sto = open(os.path.join(os.path.expanduser("~"), ".bash_history")).read()
     stl = str(sto).split("\n")
 
     if limitnum is not None:
@@ -71,79 +67,101 @@ def main():
     if colorize_from < 0:
         colorize_from = 0
 
-    hist_item_cnt = 0
+    hcnt = 0
+    if runid is True:
+        for cnt, history_item in enumerate(stl):
+            if len(history_item.strip()) > 0:
+                hcnt += 1
+                if hcnt == keyword:
+                    print("\033[30m" + history_item + "\033[0m")
+                    # script = """
+                    #     shopt -s expand_aliases
+                    #     source ~/.bash_profile
+                    #     """
+                    # script += history_item
+                    # script = "".join([x.strip()+"\n" for x in script.split("\n")]).strip()
+                    # f = open("hscript.sh", "w")
+                    # f.write(script)
+                    # f.close()
+                    #os.chmod("hscript.sh", stat.S_IREAD | stat.S_IWRITE | stat.S_IEXEC)
+                    #os.system("./hscript.sh; rm ./hscript.sh")
+                    os.system(history_item)
 
-    for cnt, history_item in enumerate(stl):
-        hist_item_cnt += 1
 
-        if len(history_item.strip()) > 0 and hist_item_cnt > start:
-            m = hashlib.md5()
-            hil = history_item.lstrip().split(" ")
+    else:
+        for cnt, history_item in enumerate(stl):
+            if len(history_item.strip()) > 0:
+                hcnt += 1
+                m = hashlib.md5()
+                hil = history_item.lstrip().split(" ")
+                m.update(str(hil[0:][0]).encode())
 
-            m.update(str(hil[1:][0]).encode())
+                # if m.hexdigest() not in st:
+                command = " ".join(history_item.lstrip().split(" ")).strip()
 
-            # if m.hexdigest() not in st:
-            command = " ".join(history_item.lstrip().split(" ")[1:]).strip()
-            num = " ".join(history_item.lstrip().split(" ")[:1]).strip()
-            dist, diff = get_distance(command, previous_command)
-            maxdist = 6
+                if keyword is not None and runid is False:
+                    if keyword not in command:
+                        command = ""
 
-            if len(command) < maxdist + 1:
-                maxdist = len(command) // 2
+                if len(command.strip()) > 0:
+                    num = str(hcnt)
+                    dist, diff = get_distance(command, previous_command)
+                    maxdist = 6
 
-            if "".join(command.split()[:2]) in prev_cmds:
-                if prev_cmds.count("".join(command.split()[:2])) < 2:
-                    print_item(num, command, showid, 30)
+                    if len(command) < maxdist + 1:
+                        maxdist = len(command) // 2
 
-                # if prev_cmds.count(command.split()[:3]) < 6:
-                #    print_item(num, command, showid, 90)
-                else:
-                    pass
+                    if "".join(command.split()[:2]) in prev_cmds:
+                        if prev_cmds.count("".join(command.split()[:2])) < 2:
+                            print_item(num, command, showid, 30)
+                        else:
+                            pass
 
-            elif 0 < dist < maxdist:
-                print_item(num, command, showid, 90)
-            else:
-                if len(command) > 160:
-                    cmdt = ""
-                    cnt = 0
+                    elif 0 < dist < maxdist:
+                        print_item(num, command, showid, 90)
+                    else:
+                        if len(command) > 160:
+                            cmdt = ""
+                            linebreakcnt = 0
 
-                    for command_item in command:
-                        if cnt > 100:
-                            cnt = 0
-                            cmdt += "\\\n"
+                            for command_item in command:
+                                if linebreakcnt > 100:
+                                    linebreakcnt = 0
+                                    cmdt += "\\\n"
 
-                        cmdt += command_item
+                                cmdt += command_item
 
-                        # cmdt += " "
-                        cnt += 1
+                                # cmdt += " "
+                                linebreakcnt += 1
 
-                    command = cmdt.replace("\t  \\\n", "")
-                    print_item(num, command, showid, 93)
-                else:
-                    print_item(num, command, showid, 93)
+                            command = cmdt.replace("\t  \\\n", "")
+                            print_item(num, command, showid, 93)
+                        else:
+                            print_item(num, command, showid, 93)
 
-            previous_command = command
+                    previous_command = command
 
-            while len(prev_cmds) > 10:
-                prev_cmds.popleft()
+                    while len(prev_cmds) > 10:
+                        prev_cmds.popleft()
 
-            if hist_item_cnt > colorize_from:
-                prev_cmds.append("".join(command.split()[:2]))
+                    if cnt > colorize_from:
+                        prev_cmds.append("".join(command.split()[:2]))
 
-def print_item(id, command, showid, colorcode):
+
+def print_item(mid, command, showmid, colorcode):
     """
-    @type id: str
+    @type mid: str
     @type command: str
-    @type showid: bool
+    @type showmid: bool
     @type colorcode: int
     @return: None
     """
-    idcolor = 37
+    midcolor = 37
     if colorcode != 93:
-        idcolor = 90
+        midcolor = 90
 
-    if showid is True:
-        print("\033[" + str(idcolor) + "m" + str(id) + "  \033[" + str(colorcode) + "m" + command, "\033[0m")
+    if showmid is True:
+        print("\033[" + str(midcolor) + "m" + str(mid) + "  \033[" + str(colorcode) + "m" + command, "\033[0m")
     else:
         print("\033[" + str(colorcode) + "m" + command, "\033[0m")
 
