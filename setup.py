592a64041860c74e842048dd13912f8cc9e83a16
Erik de Jonge <erik@a8.nl>
2015-06-03 13:41:55 +0200

    Ludwig Mies van der Rohe: God is in the details. Wednesday 03 June 2015 (week:22 day:154), 13:41:54

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ license: GNU-GPL2
 
 from setuptools import setup
 setup(name='historybash',
-      version='48',
+      version='49',
       description='Bash history command colorized on levenshtein distance of last 10 commands',
       url='https://github.com/erikdejonge/historybash',
       author='Erik de Jonge',
