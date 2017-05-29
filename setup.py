2afa296a4049c91f2859260a60774c76dd3bef7e
Erik de Jonge <erik@a8.nl>
2017-05-29 14:28:47 +0200

    Monday 29 May 2017 (week:22 day:149), 14:23:22

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ author: erik@a8.nl
 """
 from setuptools import setup
 setup(name='historybash',
-      version='82',
+      version='83',
       description='Bash history command colorized on levenshtein',
       url='https://github.com/erikdejonge/historybash',
       author='Erik de Jonge',
